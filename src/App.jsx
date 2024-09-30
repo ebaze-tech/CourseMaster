9c6cd0060ee2d470833ea8c82369ec27a762d9d0
ebaze-tech <ebaze.technologies@gmail.com>
2024-09-30 16:32:19 +0100

    Inteface changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-// import { AuthProvider } from "./Admin/AuthContext";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginForm from "./TestUsers/LoginPage";
 import RegisterForm from "./TestUsers/RegisterForm";
 import CategorySelection from "./TestUsers/CategorySelection";
 import SubmittedTests from "./TestUsers/SubmittedTests";
-import AdminTestView from "./Admin/AdminSubmittedTests";
-import TestDetails from "./Admin/AdminTestDetails";
 import "./App.css";
-import AdminLoginForm from "./Admin/AdminLogin";
-import AdminDashboard from "./Admin/AdminDashboard";
-import AdminRegisterForm from "./Admin/AdminRegister";
-import Leaderboard from "./Admin/Leaderboard";
-import AdminTestUpload from "./Admin/AdminTestUpload";
 import TestForm from "./TestUsers/TestPage";
 import UserDashboard from "./TestUsers/Userdashboard";
 import Homepage from "./TestUsers/Homepage";
 import ViewTest from "./TestUsers/ViewTest";
-import AdminHomePage from "./Admin/AdminHomepage";
+import NotFoundPage from "./TestUsers/404Page";
 
 function App() {
   return (
@@ -31,19 +18,11 @@ function App() {
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/test/category" element={<CategorySelection />} />
         <Route path="/test/new" element={<TestForm />} />
-        <Route path="/admin/tests" element={<AdminTestView />} />
-        <Route path="/admin/tests/:id" element={<TestDetails />} />
-        <Route path="/login/admin" element={<AdminLoginForm />} />
-        <Route path="/register/admin" element={<AdminRegisterForm />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="*" element={<Navigate to="/login" />} />
-        <Route path="/admin/leaderboard" element={<Leaderboard />} />
-        <Route path="/admin/test/upload" element={<AdminTestUpload />} />
+        <Route path="*" element={<NotFoundPage />} />
         <Route path="/user/dashboard" element={<UserDashboard />} />
         <Route path="/user/test/result/all" element={<SubmittedTests />} />
         <Route path="/user/test/result/:id" element={<ViewTest />} />
         <Route path="/" element={<Homepage />} />
-        <Route path="/admin" element={<AdminHomePage />} />
       </Routes>
     </Router>
   );
--- src/TestUsers/LoginPage.jsx
+++ src/TestUsers/LoginPage.jsx
@@ -54,7 +54,7 @@ const LoginForm = () => {
         <motion.div
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1.0}}
+          transition={{ duration: 1.0 }}
         >
           <Typography
             variant="h4"
