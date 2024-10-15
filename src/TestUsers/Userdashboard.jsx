09f8f308e30e2d7d5363b4095426d4ea49c0dc05
ebaze-tech <ebaze.technologies@gmail.com>
2024-10-15 16:29:41 +0100

    Continuing user dashboard

--- src/TestUsers/Userdashboard.jsx
+++ src/TestUsers/Userdashboard.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import "./Userdashboard.css";
+import UILOGO from "../assets/"
 const Userdashboard = () => {
-  return <div className="dashboard-container">User Dashbboard</div>;
+  return (
+    <div className="dashboard-container">
+      {/* LEFT SECTION */}
+      <section>
+        <div>
+          <img src={UILOGO} alt="" />
+        </div>
+        <div></div>
+      </section>
+
+      {/* MIDDLE SECTION */}
+      <section></section>
+
+      {/* RIGHT SECTION */}
+      <section></section>
+    </div>
+  );
 };
 
 export default Userdashboard;
